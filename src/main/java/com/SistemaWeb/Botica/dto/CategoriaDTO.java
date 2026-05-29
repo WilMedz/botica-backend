38b1f7c258b96bbb6e889f332948003a06ad2c1c
Santiago Lumba <santiago321lumbarojas456@gmail.com>
2026-05-28 20:55:02 -0500

    actualizacion del CategoriaDTO

--- src/main/java/com/SistemaWeb/Botica/dto/CategoriaDTO.java
+++ src/main/java/com/SistemaWeb/Botica/dto/CategoriaDTO.java
@@ -2,12 +2,16 @@ package com.SistemaWeb.Botica.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@Relation(collectionRelation = "categorias")
 public class CategoriaDTO extends RepresentationModel<CategoriaDTO> {
     private Integer idCategoria;
     private String nombre;
