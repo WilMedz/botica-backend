1d21e3a5c095d803763397f72a73fa0579f9a7aa
WilMedz <wilmamediza22@gmail.com>
2026-06-18 11:06:55 -0500

    se agregó anotación Query

--- src/main/java/com/SistemaWeb/Botica/repository/IMenuRepository.java
+++ src/main/java/com/SistemaWeb/Botica/repository/IMenuRepository.java
@@ -1,7 +1,12 @@
 package com.SistemaWeb.Botica.repository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 import com.SistemaWeb.Botica.model.Menu;
 
 public interface IMenuRepository extends IGenericRepository<Menu, Integer> {
+    @Query("FROM Menu m JOIN m.roles r WHERE r.nombre = :nombreRol")
+    List<Menu> findMenusByRol(@Param("nombreRol") String nombreRol);
 
 }
