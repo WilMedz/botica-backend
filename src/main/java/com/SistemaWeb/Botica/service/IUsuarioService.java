712ff92a267880be6f895d81809f3fa6938e4f30
Santiago Lumba <santiago321lumbarojas456@gmail.com>
2026-06-18 15:39:10 -0500

    Correción de IUsuarioService

--- src/main/java/com/SistemaWeb/Botica/service/IUsuarioService.java
+++ src/main/java/com/SistemaWeb/Botica/service/IUsuarioService.java
@@ -3,5 +3,6 @@ package com.SistemaWeb.Botica.service;
 import com.SistemaWeb.Botica.model.Usuario;
 
 public interface IUsuarioService extends IGenericService<Usuario,Integer> {
-
+    Usuario findOneByUsername(String username);
+    void changePassword(String username, String password);
 }
