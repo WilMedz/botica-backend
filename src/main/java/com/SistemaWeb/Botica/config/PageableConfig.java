02d1a89131e4ffeeee4265528955824eb2be3b02
WilMedz <wilmamediza22@gmail.com>
2026-07-02 11:54:54 -0500

    configuración

--- src/main/java/com/SistemaWeb/Botica/config/PageableConfig.java
+++ src/main/java/com/SistemaWeb/Botica/config/PageableConfig.java
@@ -1,5 +1,10 @@
 package com.SistemaWeb.Botica.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class PageableConfig {
 
 }
